c7e00384ec894b3e213354e0e0116ee87b21e438
TheDasher1 <abhijeetprajapati85@gmail.com>
2022-05-26 13:27:58 -0400

    python messed up, scraping this project

--- src/webChat.py
+++ src/webChat.py
@@ -1,6 +1,10 @@
+from turtle import title
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'f0db6bf5254931c37d94eb60cf9bf80d611c9ca9f425ca506ea6d95da45f211b'
 
 posts = [
     {
@@ -22,6 +26,18 @@ posts = [
 @app.route("/home")
 def hello():
     return render_template('home.html', title='My Flask Blog', postsArgs=posts)
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+
+    return render_template('login.html', title='Login', form=form)
 
 @app.route("/about")
 def about():
